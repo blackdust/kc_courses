b68e5fcecb07aba547467525c787697e5ea2b297
libochao <522427432@qq.com>
2016-07-29 17:10:11 +0800

    收藏项

--- lib/kc_courses/engine.rb
+++ lib/kc_courses/engine.rb
@@ -15,6 +15,22 @@ module KcCourses
         has_many :course_joins, class_name: 'KcCourses::CourseJoin'
         has_many :ware_readings, class_name: 'KcCourses::WareReading'
         has_many :ware_reading_deltas, class_name: 'KcCourses::WareReadingDelta'
+        has_many :favorites, class_name: 'KcCourses::Favorite'
+
+        define_method :set_favorite_course do |course|
+          unless self.favorites.where(course: course).any?
+            self.favorites.create course: course
+          end
+        end
+
+        define_method :cancel_favorite_course do |course|
+          self.favorites.where(course: course).destroy_all > 0 ? true : false
+        end
+
+        define_method :favorite_courses do |&block|
+          self.favorites.map{|x| x.course}
+        end
+
 
         define_method :join_course do |course|
           return if course.class.name != 'KcCourses::Course'
